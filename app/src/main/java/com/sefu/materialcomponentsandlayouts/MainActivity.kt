c5a357122981e8fe0b188683d4c42b8f287e4edb
Templa-io <humphreywritter@gmail.com>
2022-08-06 12:36:42 +0300

    Create Top Bar Composable

--- app/src/main/java/com/sefu/materialcomponentsandlayouts/MainActivity.kt
+++ app/src/main/java/com/sefu/materialcomponentsandlayouts/MainActivity.kt
@@ -3,13 +3,23 @@ package com.sefu.materialcomponentsandlayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sefu.materialcomponentsandlayouts.ui.theme.MaterialComponentsANdLayoutsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +27,56 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialComponentsANdLayoutsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
 
-                }
+            }
+        }
+
+
+    }
+}
+
+@Composable
+fun TopBar(modifier: Modifier = Modifier) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(top = 24.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+            .fillMaxWidth()
+    ) {
+        Column() {
+            Text(text = "Discover", fontWeight = FontWeight.Bold, fontSize = 28.sp)
+            Text(text = "new tasting experience", color = Color.Gray)
+        }
+        Box(
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(12.dp))
+                .size(60.dp)
+                .background(color = Color.Red),
+            contentAlignment = Alignment.Center
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(alignment = Alignment.Center),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Icon(Icons.Rounded.Settings, contentDescription = "filter", tint = Color.White)
+                Text(
+                    text = "Filter",
+                    fontWeight = FontWeight.Bold,
+                    style = TextStyle(color = Color.White)
+                )
             }
         }
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun TopBarPriview() {
+    MaterialComponentsANdLayoutsTheme {
+        TopBar()
+    }
+}
